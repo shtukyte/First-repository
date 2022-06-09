dc80ef17ebc19f9c255df28f4c242d1fb66d3d38
shtukyte <matulionisk@gmail.com>
2022-06-09 18:46:39 +0300

    task_06-09

---
+++ task/script.js
@@ -0,0 +1,84 @@
+// Testuosime šį masyvą
+let numbers = [5, 1, 7, 2, -9, 8, 2, 7, 9, 4, -5, 2, -6, -4, 6];
+// 1. Parašykite funkciją arrDoubled, kuri sukuria ir grąžina naują masyvą, kurio elementai padauginti iš 2;
+// function arrDoubled (numbers){
+// return numbers.map(x => x*2);
+// }
+// console.log (arrDoubled(numbers));
+
+// 2. Parašykite funkciją arrMultiplied, kuri sukuria ir grąžina naują masyvą, kurio elementai padauginti iš argumentu nurodyto skaičiaus
+// function arrMultiplied (numbers, arrMultiNumber){
+//   return numbers.map(x => x*arrMultiNumber);
+// }
+// console.log(arrMultiplied(numbers, 3))
+
+// 3. Parašykite funkciją arrCountTwos, kuri suskaičiuoja dvejetus masyve
+// let result = numbers.filter(i=> i === 2).length
+// console.log (result);
+
+// 4. Parašykite funkciją arrIndexOfFirst, kuri grąžintu pirmo surasto, argumentu nurodyto skaičiaus, indeksą masyve. Jei skaičius nerastas funkcija turi grąžinti -1.
+// function arrIndexOfFirst (numbers, x) {
+//   for (let i=0; i < numbers.lenght; i++) {
+//     if (numbers[i] == x) {
+//       return i;
+//     }
+//     }
+//   }
+//     console.log(arrIndexOfFirst(numbers, 5));
+// 5. Parašykite funkciją arrIndexOfLast, kuri grąžintu paskutinio surasto, argumentu nurodyto skaičiaus, indeksą masyve. Jei skaičius nerastas funkcija turi grąžinti -1.
+
+
+// 6. Parašykite funkciją reverseNumbers, kuri pakeis skaičius vietomis taip, kad pirmas skaičius taps paskutiniu, antras piešpaskutiniu, o buvęs paskutinis taps pirmu, priešpaskutinis bus antru.
+// Pvz.: Turime skaičius 32243;
+// Iškvietus funkciją rezultata bus: 34223
+
+// 7. Parašykite  funkciją, kuri kaip argumentą priims skaičių masyvą ir suras atitinkamai mažiausią ir didžiausią skaičių bei juos grąžins.
+// Pvz.: Turime masyvą: [8,5,4,2,7,1,9]
+// Iškvietus funkciją rezultata bus: "Mažiausas: 1, Didžiausas: 9"
+// function getMinNumber (numbers) {
+//   return (Math.min(...numbers))
+// }
+
+// function getMaxNumber (numbers) {
+//   return (Math.max(...numbers))
+// }
+
+// console.log(getMinNumber(numbers));
+// console.log(getMaxNumber(numbers));
+// 8. Parašykite  funkciją checkForLetters, kuri priims du argumentus: Pirmas argumentas bus sakinys (arba žodžiai (-is)) ir antras argumentas bus raidė (kaip string). Funkcija turės suskaičiuoti kiek pirmu agrumentu nurodytame sakinyje/žodžiuose(-yje) yra antru argumentu nurodytų raidžių ir gražinti tų raidžių sumą su sakiniu pvz.: “Raidė “v” sakinyje rasta 4 kartus”.
+
+// 9. Parašykite funckiją, kuri ras visus skaičius esančius msyve ir gražins juos kaip atsikrą masyvą. Naujame masyve visi skaičiai bus surikiuoti nuo mažiausio iki didžiausio.
+// let arr2 = [8, 'Hello', 'click', 'u', 7, 4, 'a', 'a', 3, 6, "chair", ,10, 1];
+// Iškvietus funkciją rezultata bus: [1,3,4,6,7,8,10];
+
+// 10. Sukurkite funkciję checkIfAllSmaller(arr, max), BE MASYVO METODŲ, kuri patikrintų ar visi skaičiai masyve yra didesni negu perduota reikšmė max;
+// Pvz.: Turime masyvą: let arr1 = [2, 7, 6, 9, 5];
+// Iškvietus funkciją checkIfAllSmaller(arr1, 5) rezultata bus: False
+
+// 11. Parašykite funkciją filteredByLetter, kuri turi du parametrus: 1. masyvas; 2. raidė. Funkcija sukuria ir grąžina naują masyvą, kuriame yra visi masyvo, nurodyto kaip pirmas parametras elemntai, kuriuose galima rasti antru paramatetru nurodytą raidę.
+// Testuosime šį masyvą
+let citiesOfLithuania = [
+  'Vilnius',
+  'Kaunas',
+  'Klaipėda',
+  'Šiauliai',
+  'Panevėžys',
+  'Alytus',
+  'Marijampolė',
+  'Mažeikiai',
+  'Jonava',
+  'Utena',
+];
+
+// 12. Parašykite penkias funkcijas:
+// - calculateValue()
+// - addition()
+// - subtraction()
+// - multiplication()
+// - division()
+
+// Pagridinė funkcija bus calculateValue(num1, num2, action), kuri priims tris argumentus: a) num1 - skaičius;b) num2 - skaičius; c) action - (matematinis veiksmas kaip string pvz. “substraction”). Būtina, kad funckija validuotų ar num1 ir num2 yra skaičiai.
+
+// Priklausomai trečio argumento (action), su pirmais dviem (num1 ir num2) bus atliekamas matematinis veiksmas ir kviečiama viena iš keturių funkcijų, kurios priims du argumentus (num1 ir num2) ir grąžins atlikta veiksmą.
+
+// Pastaba: šios funkcijos: addition(), subtraction(), multiplication(), division() turi būti kviečiamas calculateValue() viduje, o aprašomos išorėje.
---
+++ task2022-06-09/app.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById ('age'))
+console.log(document.getElementById ('age-table'))
+console.log(document.getElementById ('form'))
+console.log(document.querySelector("input[name=name]"));
+console.log(document.querySelector("input[name=phoneNum]"));
