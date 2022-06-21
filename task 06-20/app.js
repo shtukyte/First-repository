39710f8f7458faf6edf33f1472ba212c83303acb
shtukyte <matulionisk@gmail.com>
2022-06-21 21:03:09 +0300

    fix

--- task 06-20/app.js
+++ task 06-20/app.js
@@ -9,8 +9,8 @@ function showTime(){
     s = (s < 10) ? "0" + s : s;
     
     let time = h + ":" + m + ":" + s + " ";
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
+    document.getElementById("myClockDisplay").innerText = time;
+    document.getElementById("myClockDisplay").textContent = time;
     
     setTimeout(showTime, 1000);
     
@@ -40,18 +40,19 @@ let firstName = document.querySelector('#fName');
 let lastName = document.querySelector('#lName');
 let phoneNumber = document.querySelector('#phoneNumber');
 let emailAdress = document.querySelector('#emailAdress');
-let message = 'Form submited successfully';
+let confirmMessage = 'Form submited successfully';
+let failMessage = 'Form submited successfully';
 
-function subFormChecker() {
-    if (firstName.value == "" || lastName.value == "" ||
-        phoneNumber.value == "" || emailAdress.value == "") {
-            alert('Please fill all fields!');
-            return false;
-        }
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-
-
-    }
+  if (firstName.value == '' || lastName.value == '' || phoneNumber.value == '' || emailAdress.value == '') {
+      alert ('Fill all fields ');
+  } else {
+      alert ('Form submit succesfully');
+ 
+  }
+});
 
 
 
